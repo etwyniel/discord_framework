4f8c157f5e97b64cc5a527b7323a04a6c22733c7
Aymeric Beringer <aymericberin@gmail.com>
2023-07-17 22:17:51 +0200

    Edit ready poll message in separate task, add comments

--- serenity-command-handler/src/modules/ready_poll.rs
+++ serenity-command-handler/src/modules/ready_poll.rs
@@ -6,6 +6,7 @@ use std::time::{Duration, Instant};
 use anyhow::{anyhow, Context as _};
 use itertools::Itertools;
 use serenity::http::Http;
+use serenity::model::application::interaction::MessageInteraction;
 use serenity::model::id::InteractionId;
 use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::prelude::{ChannelId, Message, Reaction, ReactionType, UserId};
@@ -45,10 +46,11 @@ impl ReadyPoll {
         handler: &Handler,
         ctx: &Context,
         interaction: &ApplicationCommandInteraction,
-    ) -> anyhow::Result<CommandResponse> {
+    ) -> anyhow::Result<()> {
         let pending_poll = (interaction.id, self.count_emote, self.go_emote, Vec::new());
         let module: &ModPoll = handler.module()?;
         let http = &ctx.http;
+        // create initial response to the interaction
         interaction
             .create_interaction_response(http, |msg| {
                 msg.interaction_response_data(|data| {
@@ -58,15 +60,23 @@ impl ReadyPoll {
             })
             .await
             .context("error creating response")?;
+
+        // retrieve handle to interaction response so we can edit it later
         let resp = interaction.get_interaction_response(http).await?;
+        // create async channel in order to process reactions asynchronously
         let (sender, receiver) = channel(16);
+
         {
+            // add this poll to the list
+            // using a sub-scope to ensure write lock gets dropped ASAP
             let mut polls = module.ready_polls.write().await;
             while polls.len() >= MAX_POLLS {
                 polls.pop_back();
             }
             polls.push_front((interaction.id, sender));
         }
+
+        // add reacts to interaction response
         resp.react(http, ReactionType::from_str(&module.yes)?)
             .await
             .context("error adding yes react")?;
@@ -76,6 +86,8 @@ impl ReadyPoll {
         resp.react(http, ReactionType::from_str(&module.start)?)
             .await
             .context("error adding go react")?;
+
+        // spawn task to handle reactions
         let http_arc = Arc::clone(&ctx.http);
         tokio::spawn(poll_task(
             handler.module_arc().unwrap(),
@@ -84,10 +96,13 @@ impl ReadyPoll {
             pending_poll,
             receiver,
         ));
-        Ok(CommandResponse::None)
+        // already created a response
+        Ok(())
     }
 }
 
+// build ready poll message.
+// lists users that have clicked the YES react as being ready.
 fn build_message(users: &[UserId]) -> String {
     let mut msg = "Ready?".to_string();
     if users.is_empty() {
@@ -114,14 +129,15 @@ impl BotCommand for ReadyPoll {
         ctx: &Context,
         interaction: &ApplicationCommandInteraction,
     ) -> anyhow::Result<CommandResponse> {
+        // create ready poll message
         let resp = match self.create_poll(handler, ctx, interaction).await {
-            Ok(CommandResponse::Public(s) | CommandResponse::Private(s)) => Some(s),
             Err(e) => {
                 dbg!(&e);
                 Some(e.to_string())
             }
             _ => None,
         };
+        // in case creating the poll failed, try to edit the interaction response with an error message
         if let Some(resp) = resp {
             interaction
                 .edit_original_interaction_response(&ctx.http, |msg| {
@@ -136,21 +152,25 @@ impl BotCommand for ReadyPoll {
 enum PollEvent {
     AddReady(UserId),
     RemoveReady(UserId),
-    Go,
+    Start,
 }
 
+// task responsible for handling reactions to a poll
 async fn poll_task(
     module: Arc<ModPoll>,
     http: Arc<Http>,
-    mut msg: Message,
+    msg: Message,
     poll: PendingPoll,
     mut r: Receiver<PollEvent>,
 ) {
-    let mut users = Vec::new();
-    let mut changed = false;
-    let mut started = false;
+    // poll state
+    let mut users = Vec::new(); // list of users who have clicked the READY react
+    let mut changed = false; // whether the message needs to be edited
+    let mut started = false; // whether the poll's author has clicked the GO react
     let mut last_event = Instant::now();
+
     loop {
+        // poll for new events
         while let Ok(evt) = timeout(Duration::from_millis(500), r.recv()).await {
             let Some(evt) = evt else {
                 // channel closed
@@ -165,11 +185,9 @@ async fn poll_task(
                     }
                 }
                 PollEvent::RemoveReady(user) => {
-                    if let Some(ndx) = users.iter().position(|&u| u == user) {
-                        users.remove(ndx);
-                    }
+                    users.retain(|&u| u != user);
                 }
-                PollEvent::Go => {
+                PollEvent::Start => {
                     if started {
                         continue;
                     }
@@ -189,24 +207,34 @@ async fn poll_task(
             }
         }
         if last_event.elapsed() >= Duration::from_secs(900) {
+            // too long since last event, stop this task
             return;
         }
         if !changed {
+            // no change, no need to edit the message
             continue;
         }
         let content = build_message(&users);
-        let res = msg
-            .edit(http.as_ref(), |msg| {
-                msg.content(content).allowed_mentions(|m| m.empty_users())
-            })
-            .await;
-        if let Err(e) = res {
-            eprintln!("failed to edit ready message: {e}");
-        }
+        // edit message in a separate task to avoid blocking this one
+        tokio::spawn({
+            let http = Arc::clone(&http);
+            let mut msg = msg.clone();
+            async move {
+                let res = msg
+                    .edit(http.as_ref(), |msg| {
+                        msg.content(content).allowed_mentions(|m| m.empty_users())
+                    })
+                    .await;
+                if let Err(e) = res {
+                    eprintln!("failed to edit ready message: {e}");
+                }
+            }
+        });
         changed = false;
     }
 }
 
+// performs the actual countdown
 pub async fn crabdown(
     module: Arc<ModPoll>,
     http: &Http,
@@ -214,23 +242,24 @@ pub async fn crabdown(
     count_emote: Option<&str>,
     go_emote: Option<&str>,
 ) -> anyhow::Result<()> {
+    // announce countdown is starting, wait briefly
     channel.say(http, "Starting 3s countdown").await?;
     tokio::time::sleep(Duration::from_secs(2)).await;
+
+    // use interval instead of sleep to minimize drift due to the time it takes to send a message
     let mut interval = tokio::time::interval(Duration::from_secs(1));
+    // first tick happens with no delay, skip it
     interval.tick().await;
-    // let module: &ModPoll = handler.module()?;
+
     let count_emote = count_emote.unwrap_or(&module.count);
     let go_emote = go_emote.unwrap_or(&module.go);
     for i in 0..3 {
+        // repeat count emote 3 - i times
         let contents = std::iter::repeat(count_emote).take(3 - i).join(" ");
-        channel
-            .send_message(http, |msg| msg.content(contents))
-            .await?;
+        channel.say(http, contents).await?;
         interval.tick().await;
     }
-    channel
-        .send_message(http, |msg| msg.content(go_emote))
-        .await?;
+    channel.say(http, go_emote).await?;
     Ok(())
 }
 
@@ -246,6 +275,8 @@ pub struct ModPoll {
 }
 
 impl ModPoll {
+    // instantiate the module with any number of emotes specified.
+    // unspecified emotes will use the default.
     pub fn new<
         'a,
         S1: Into<Option<&'a str>>,
@@ -270,35 +301,53 @@ impl ModPoll {
         }
     }
 
+    // returns None if msg is not a /ready_poll interaction response
+    fn get_poll_interaction<'a>(
+        handler: &Handler,
+        msg: &'a Message,
+    ) -> Option<&'a MessageInteraction> {
+        // check if we sent this message
+        if Some(&msg.author.id) != handler.self_id.get() {
+            // not ours
+            return None;
+        }
+        // check if this message is a response to a /ready_poll command
+        msg.interaction
+            .as_ref()
+            .filter(|interaction| interaction.name == ReadyPoll::NAME)
+    }
+
+    // callback for react removal
     pub async fn handle_remove_react(
         handler: &Handler,
         ctx: &Context,
         react: &Reaction,
     ) -> anyhow::Result<()> {
         let msg = react.message(&ctx.http).await?;
-        if Some(&msg.author.id) != handler.self_id.get() {
+        let Some(interaction) = Self::get_poll_interaction(handler, &msg) else {
             return Ok(());
-        }
-        let (interaction_id, _) = match msg.interaction.as_ref() {
-            Some(interaction) if interaction.name == ReadyPoll::NAME => {
-                (interaction.id, interaction.user.id)
-            }
-            _ => return Ok(()),
         };
+
+        // we only care about YES reacts being removed
         let module: &ModPoll = handler.module()?;
         if react.emoji.to_string() != module.yes {
             return Ok(());
         }
+
+        // get the ID of the user who removed the react
         let user_id = react
             .user_id
             .ok_or_else(|| anyhow!("invalid react: missing userId"))?;
+
+        // find the sender for that poll's handler and send a RemoveReady event
         let polls = module.ready_polls.read().await;
-        if let Some((_, sender)) = polls.iter().find(|(id, _)| *id == interaction_id) {
+        if let Some((_, sender)) = polls.iter().find(|(id, _)| *id == interaction.id) {
             _ = sender.send(PollEvent::RemoveReady(user_id)).await;
         }
         Ok(())
     }
 
+    // callback for adding a react
     pub async fn handle_ready_poll(
         handler: &Handler,
         ctx: &Context,
@@ -306,37 +355,36 @@ impl ModPoll {
     ) -> anyhow::Result<()> {
         let http = &ctx.http;
         let msg = react.message(http).await?;
-        let (interaction_id, interaction_user) = match msg.interaction.as_ref() {
-            Some(interaction) if interaction.name == ReadyPoll::NAME => {
-                (interaction.id, interaction.user.id)
-            }
-            _ => return Ok(()),
+        let Some(interaction) = Self::get_poll_interaction(handler, &msg) else {
+            return Ok(());
         };
-        let module: &ModPoll = handler.module()?;
+
+        // get the ID of the user who added the react
         let user_id = react
             .user_id
             .ok_or_else(|| anyhow!("invalid react: missing userId"))?;
-        if react.emoji.to_string() == module.yes && Some(&user_id) != handler.self_id.get() {
-            {
-                let polls = module.ready_polls.read().await;
-                if let Some((_, sender)) = polls.iter().find(|(id, _)| *id == interaction_id) {
-                    let _ = sender.send(PollEvent::AddReady(user_id)).await;
-                }
-            }
-            return Ok(());
+
+        let module: &ModPoll = handler.module()?;
+        let event =
+            if react.emoji.to_string() == module.yes && Some(&user_id) != handler.self_id.get() {
+                // user added a YES react (and is not the bot)
+                // send AddReady event
+                PollEvent::AddReady(user_id)
+            } else if interaction.user.id == user_id && react.emoji.to_string() == module.start {
+                // poll author clicked the START react
+                // send Start event
+                PollEvent::Start
+            } else {
+                // not a react we care about
+                return Ok(());
+            };
+
+        // send event to the poll's handler task
+        let polls = module.ready_polls.read().await;
+        if let Some((_, sender)) = polls.iter().find(|(id, _)| *id == interaction.id) {
+            let _ = sender.send(event).await;
         }
-        if interaction_user != user_id {
-            return Ok(());
-        }
-        if react.emoji.to_string() != module.start {
-            return Ok(());
-        }
-        {
-            let polls = module.ready_polls.read().await;
-            if let Some((_, sender)) = polls.iter().find(|(id, _)| *id == interaction_id) {
-                let _ = sender.send(PollEvent::Go).await;
-            }
-        }
+
         Ok(())
     }
 }
